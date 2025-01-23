273db9817739a3d0dee3673261a3e8d4dc3fb40d
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-23 20:56:57 +0530

    react-component to image export feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from 'react-component-export-image';
+import React, { useRef } from 'react';
+import { GithubGraph } from './components/ui/github';
 
-function App() {
+
+const App = () => {
+  const componentRef = useRef();
+
   return (
-    <>
-      <h1 className="underline">hello</h1>
-    </>
-  )
-}
+    <React.Fragment>
+      <div ref={componentRef}>
+        <GithubGraph username="benedicti0n" />
+      </div>
+      <button onClick={() => exportComponentAsJPEG(componentRef)}>
+        Export As JPEG
+      </button>
+      <button onClick={() => exportComponentAsPDF(componentRef)}>
+        Export As PDF
+      </button>
+      <button onClick={() => exportComponentAsPNG(componentRef)}>
+        Export As PNG
+      </button>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
