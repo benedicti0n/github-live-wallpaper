c4e6e329cd160f8e007c536936f155a1c0a8a205
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-25 00:19:44 +0530

    input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,22 @@
+const Input = () => {
+    return (
+        <div className=" flex justify-center items-center relative rounded-xl transition-shadow duration-300 ease-in-out focus-within:shadow-lg focus-within:shadow-blue-700/60">
+            {/* Outer gradient ring */}
+            <div className="absolute inset-0 bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 rounded-xl p-1">
+                {/* Inner container for ring */}
+                <div className="bg-[#e8e8e8] rounded-lg h-full w-full"></div>
+            </div>
+
+            {/* Actual Input Field */}
+            <input
+                type="text"
+                name="text"
+                placeholder="Search here..."
+                autoComplete="off"
+                className="relative bg-transparent text-zinc-600 outline-none placeholder:text-zinc-600 placeholder:opacity-50 px-4 py-2 w-full focus:ring-0 focus:outline-none"
+            />
+        </div>
+    );
+};
+
+export default Input;
