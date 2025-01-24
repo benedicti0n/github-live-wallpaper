af6aaad10cd33469d244dc15879b3ae9c1cc75ef
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-25 00:19:30 +0530

    minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Sparkle from '../icons/Sparkle'
 
 const Navbar = () => {
     return (
-        <div className='w-full h-18 p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60'>
-            <div className='w-full h-full flex justify-between items-center rounded-xl p-2 bg-[#e8e8e8]'>
+        <div className='w-full h-auto p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60'>
+            <div className='w-full h-18 flex justify-between items-center rounded-xl p-2 bg-[#e8e8e8]'>
                 <div className='h-full flex items-center'>
                     <img src="/logo.png" alt="" className='h-full' />
                     <h1 className='font-semibold text-blue-600'>Github Live Wallpaper</h1>
