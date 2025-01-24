89bf943a33da19ca6f9bb9e71c1b1b6957deee61
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-24 21:09:17 +0530

    color dynamic prop removed due to bug

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,10 +8,10 @@ interface ButtonProps {
 const Button = (props: ButtonProps) => {
     return (
         <button
-            className={`brightness-150 dark:brightness-100 group hover:shadow-lg hover:shadow-blue-700/60 transition ease-in-out hover:scale-105 p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 hover:from-blue-700 hover:via-blue-800 hover:to-blue-600`}
+            className={`brightness-150 dark:brightness-100 group hover:shadow-lg hover:shadow-blue-700/60 transition ease-in-out hover:scale-105 p-1 rounded-xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 hover:from-blue-700 hover:via-blue-800 hover:to-blue-600`}
         >
             <div
-                className="px-6 py-2 backdrop-blur-xl bg-[#e8e8e8] rounded-xl font-semibold w-full h-full"
+                className="px-6 py-2 backdrop-blur-xl bg-[#e8e8e8] rounded-lg font-semibold w-full h-full"
             >
                 <div
                     className={`group-hover:scale-100 flex group-hover:text-blue-500 text-blue-600 gap-1`}
