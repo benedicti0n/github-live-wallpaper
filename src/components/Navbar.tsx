34a9d266494e90be993ffa614ae3bbf94acffd17
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-24 21:35:34 +0530

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,12 @@ import Sparkle from '../icons/Sparkle'
 
 const Navbar = () => {
     return (
-        <div className='w-full p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60'>
-            <div className='w-full flex justify-between items-center rounded-xl p-2 bg-[#e8e8e8]'>
-                <img src="" alt="" />
+        <div className='w-full h-18 p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60'>
+            <div className='w-full h-full flex justify-between items-center rounded-xl p-2 bg-[#e8e8e8]'>
+                <div className='h-full flex items-center'>
+                    <img src="/logo.png" alt="" className='h-full' />
+                    <h1 className='font-semibold text-blue-600'>Github Live Wallpaper</h1>
+                </div>
                 <Button text="SignIn" icon={<Sparkle />} />
             </div>
         </div>
