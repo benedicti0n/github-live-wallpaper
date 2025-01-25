e957c91930de016ab38a43f642e4699e3527fc15
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-25 07:34:46 +0530

    3 pallete colorhues include

---
+++ src/components/ui/GithubBento/ColorHues.ts
@@ -0,0 +1,20 @@
+export const BluePallete = {
+    1: '#26b0ff',
+    2: '#2686ff',
+    3: '#265bff',
+    4: '#2630ff',
+}
+
+export const GreenPallete = {
+    1: '#a3f7b5',
+    2: '#72e0ac',
+    3: '#51bea1',
+    4: '#42a59f',
+}
+
+export const PurplePallete = {
+    1: '#efd9ce',
+    2: '#dec0f1',
+    3: '#b79ced',
+    4: '#957fef',
+}
