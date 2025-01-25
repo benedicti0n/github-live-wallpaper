1fd625f6b856ab0337acdd9ef2fc455ce3366088
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-25 05:53:12 +0530

    minor change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Sparkle from '../icons/Sparkle'
 
 const Navbar = () => {
     return (
-        <div className='w-full h-auto p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60'>
+        <div className='w-full h-auto p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60 relative'>
             <div className='w-full h-18 flex justify-between items-center rounded-xl p-2 bg-[#e8e8e8]'>
                 <div className='h-full flex items-center'>
                     <img src="/logo.png" alt="" className='h-full' />
