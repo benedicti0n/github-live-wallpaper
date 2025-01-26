7beb610ea280d377e9f0e31b4a1353ffba3b4c66
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-26 15:50:30 +0530

    minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { LucideLogIn } from "lucide-react";
 
 const Navbar = () => {
     return (
-        <div className='w-full h-auto p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60 relative'>
+        <div className='w-full h-auto p-1 rounded-2xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-lg shadow-blue-700/60 my-2'>
             <div className='w-full h-18 flex justify-between items-center rounded-xl p-2 bg-[#e8e8e8]'>
                 <div className='h-full flex items-center'>
                     <img src="/logo.png" alt="" className='h-full' />
