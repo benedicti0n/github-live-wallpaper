4f4efd1e057524dafc327031cebe29d87cbb8c43
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-26 13:16:10 +0530

    Revert "onClickFunction add"
    This reverts commit af64b92b38eb7c8f02eb5fe8f267f702d9e9c402.

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,15 +1,14 @@
 import { ReactNode } from "react";
+
 interface ButtonProps {
     text: string;
     icon?: ReactNode;
-    onClickFunction?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
     return (
         <button
             className={`brightness-150 dark:brightness-100 group hover:shadow-lg hover:shadow-blue-700/60 transition ease-in-out hover:scale-105 p-1 rounded-xl bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 hover:from-blue-700 hover:via-blue-800 hover:to-blue-600`}
-            onClick={props.onClickFunction}
         >
             <div
                 className="px-6 py-2 backdrop-blur-xl bg-[#e8e8e8] rounded-lg font-semibold w-full h-full"
