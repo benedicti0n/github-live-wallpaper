e273f9a77967fab46d9725521b2ebb1a3cd5635d
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-26 13:29:02 +0530

    Revert "app simplified"
    This reverts commit e7aa3e72155f6f4bd12b3c3b4fa1c04f7273c9a3.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
+<<<<<<< HEAD:app/page.tsx
 import Homepage from "./components/Homepage";
 
 export default function Home() {
+=======
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from 'react-component-export-image';
+import React, { useRef } from 'react';
+import { GithubGraph } from './components/ui/github';
+
+
+const App = () => {
+  const componentRef = useRef();
+
+>>>>>>> parent of e7aa3e7 (app simplified):src/App.tsx
   return (
-    <div className="w-full min-h-screen flex justify-center bg-[#e8e8e8] relative">
-      <Homepage />
-    </div>
+    <React.Fragment>
+      <div ref={componentRef}>
+        <GithubGraph username="benedicti0n" />
+      </div>
+      <button onClick={() => exportComponentAsJPEG(componentRef)}>
+        Export As JPEG
+      </button>
+      <button onClick={() => exportComponentAsPDF(componentRef)}>
+        Export As PDF
+      </button>
+      <button onClick={() => exportComponentAsPNG(componentRef)}>
+        Export As PNG
+      </button>
+    </React.Fragment>
   );
 }
