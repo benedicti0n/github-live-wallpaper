290b5b24d63ac5903ef9f4262dcbdcb8cc5a6f75
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-27 06:15:16 +0530

    Configure backend routes and middleware for GitHub stats endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,23 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, urlencoded } from "express";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
+import cors from 'cors'
+
+import fetchGithubStats from "./routes/github.route"
 
 dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(urlencoded({ extended: true }))
+
+app.use('/api/v1/', fetchGithubStats)
 
 // Sample route: Fetch GitHub stats for a user
 app.get("/", (req: Request, res: Response) => {
