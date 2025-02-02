37fc502482966894e970af135dfcf39f5a3d3fec
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-02 16:24:22 +0530

    browser router implement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
+
 const App = () => {
   return (
     <div className="w-full min-h-screen flex justify-center bg-[#e8e8e8] relative">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/preview/:username" element={<Preview />} */}
+      </Routes>
     </div>
   );
 };
