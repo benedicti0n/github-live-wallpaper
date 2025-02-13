80f4476fde66732945e3d37efebeb7949fe33da6
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-13 13:15:57 +0530

    Add the edit and delete button to the preview modal

--- frontend/src/components/Dashboard/WallpaperPreview.tsx
+++ frontend/src/components/Dashboard/WallpaperPreview.tsx
@@ -8,16 +8,23 @@ const modalOpen = atom<boolean>(false)
 const wallpaperId = '1'
 
 const PreviewModal = ({ imageUrl, closeModal }: { imageUrl: string, closeModal: () => void }) => {
+    const navigate = useNavigate()
     return (
         <div
-            className="fixed inset-0 z-50 bg-white/30 bg-opacity-75 flex justify-center items-center"
+            className="fixed inset-0 z-50 backdrop-blur-xs bg-opacity-75 flex justify-center items-center"
             onClick={closeModal}
         >
-            <div
-                className="w-2/3 h-2/3 bg-cover bg-center"
-                style={{ backgroundImage: `url(${imageUrl})` }}
-                onClick={(e) => e.stopPropagation()}
-            />
+            <div className="w-2/3 h-2/3 rounded-xl relative">
+                <div
+                    className="w-full h-full bg-cover bg-center rounded-xl"
+                    style={{ backgroundImage: `url(${imageUrl})` }}
+                    onClick={(e) => e.stopPropagation()}
+                />
+                <div className="absolute top-2 right-2 m-1 z-10">
+                    <MiniButton variant="default" className="mx-1" onClickFunction={() => { navigate(`/edit/${wallpaperId}`) }}><LucidePen className="h-6 w-6" /></MiniButton>
+                    <MiniButton variant="destructive"><LucideTrash2 className="h-6 w-6" /></MiniButton>
+                </div>
+            </div>
         </div>
     )
 }
@@ -34,7 +41,7 @@ const WallpaperPreview = ({ imageUrl }: { imageUrl: string }) => {
         <div className="w-72 h-40 rounded-xl relative mr-6">
             <img src={`${imageUrl}`} alt={`${imageUrl}`} className="bg-cover bg-center bg-no-repeat rounded-lg w-full h-full" onClick={previewWallpaper} />
             <div className="absolute top-0 right-0 m-1 z-10">
-                <MiniButton variant="default" className="mx-1" onClickFunction={() => { navigate(`/edit/${wallpaperId}`) }}><LucidePen className="h-4 w-4" /></MiniButton>
+                <MiniButton variant="default" className="mx-2" onClickFunction={() => { navigate(`/edit/${wallpaperId}`) }}><LucidePen className="h-4 w-4" /></MiniButton>
                 <MiniButton variant="destructive"><LucideTrash2 className="h-4 w-4" /></MiniButton>
             </div>
 
