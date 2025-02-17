c7029bb5b673f0116c1387c3bbf62b4947fd42cc
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-17 21:07:05 +0530

    feat: change multer storage to memory for wallpaper uploads

--- backend/src/routes/wallpaper.route.ts
+++ backend/src/routes/wallpaper.route.ts
@@ -1,10 +1,11 @@
+import multer from "multer";
 import { Router } from "express";
 const router = Router();
 
 import { saveWallpaper } from "../controllers/wallpaper.controller";
-import multer from "multer";
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.memoryStorage(); // Store file in memory instead of disk
+const upload = multer({ storage });
 
 router.post('/saveWallpaper', upload.single('image'), saveWallpaper);
 
