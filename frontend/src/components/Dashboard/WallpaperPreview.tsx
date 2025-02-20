a0c94860628b7af966d7996de81879b7d395de5c
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-21 04:25:17 +0530

    feat: integrate ConfirmDeleteModal into WallpaperPreview for deletion confirmation

--- frontend/src/components/Dashboard/WallpaperPreview.tsx
+++ frontend/src/components/Dashboard/WallpaperPreview.tsx
@@ -1,33 +1,14 @@
-import { atom, useAtom } from "jotai"
-import MiniButton from "../ui/MiniButton"
-
-import { LucidePen, LucideTrash2 } from "lucide-react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { atom, useAtom } from "jotai"
+
+import MiniButton from "../ui/MiniButton"
+import PreviewModal from "../ui/Modals/PreviewModal"
+import ConfirmDeleteModal from "../ui/Modals/ConfirmDeleteModal"
+import { LucidePen, LucideTrash2 } from "lucide-react"
 
 const modalOpen = atom<boolean>(false)
 const wallpaperId = '1'
-
-const PreviewModal = ({ imageUrl, closeModal }: { imageUrl: string, closeModal: () => void }) => {
-    const navigate = useNavigate()
-    return (
-        <div
-            className="fixed inset-0 z-50 bg-opacity-75 flex justify-center items-center"
-            onClick={closeModal}
-        >
-            <div className="w-2/3 h-2/3 rounded-xl border-2 relative">
-                <div
-                    className="w-full h-full bg-cover bg-center rounded-xl"
-                    style={{ backgroundImage: `url(${imageUrl})` }}
-                    onClick={(e) => e.stopPropagation()}
-                />
-                <div className="absolute top-2 right-2 z-10">
-                    <MiniButton variant="default" className="mx-2" onClickFunction={() => { navigate(`/edit/${wallpaperId}`) }}><LucidePen className="h-6 w-6" /></MiniButton>
-                    <MiniButton variant="destructive"><LucideTrash2 className="h-6 w-6" /></MiniButton>
-                </div>
-            </div>
-        </div>
-    )
-}
 
 const WallpaperPreview = ({ imageUrl }: { imageUrl: string }) => {
     const navigate = useNavigate()
@@ -37,13 +18,22 @@ const WallpaperPreview = ({ imageUrl }: { imageUrl: string }) => {
         setIsModalOpen((prev) => !prev)
     }
 
+    const [isDeleteConfirmModalOpen, setIsDeleteConfirmModal] = useState(false)
+
+    const openDeleteConfirmModal = () => {
+        setIsDeleteConfirmModal((prev) => !prev)
+    }
+
+
     return (
-        <div className="w-72 h-40 rounded-xl relative mr-6">
-            <img src={`${imageUrl}`} alt={`${imageUrl}`} className="bg-cover bg-center bg-no-repeat rounded-lg w-full h-full" onClick={previewWallpaper} />
+        <div className="w-72 h-40 rounded-xl relative mr-6 border-2">
+            <img src={`${imageUrl}`} alt={`${imageUrl}`} className="bg-cover bg-center bg-no-repeat rounded-xl w-full h-full" onClick={previewWallpaper} />
             <div className="absolute top-0 right-0 m-1 z-10">
                 <MiniButton variant="default" className="mx-1" onClickFunction={() => { navigate(`/edit/${wallpaperId}`) }}><LucidePen className="h-4 w-4" /></MiniButton>
-                <MiniButton variant="destructive"><LucideTrash2 className="h-4 w-4" /></MiniButton>
+                <MiniButton variant="destructive" onClickFunction={openDeleteConfirmModal}><LucideTrash2 className="h-4 w-4" /></MiniButton>
             </div>
+
+            {isDeleteConfirmModalOpen && <ConfirmDeleteModal closeModal={() => setIsDeleteConfirmModal(false)} />}
 
             {isModalOpen && <PreviewModal imageUrl={imageUrl} closeModal={() => setIsModalOpen(false)} />}
         </div>
