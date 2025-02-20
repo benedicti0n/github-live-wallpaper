eeaf17e12434e961db03551143e105d0850c1c05
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-20 06:28:04 +0530

    feat: add endpoint to fetch preview wallpapers in wallpaper route

--- backend/src/routes/wallpaper.route.ts
+++ backend/src/routes/wallpaper.route.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 const router = Router();
 
 import { saveWallpaper } from "../controllers/wallpaper.controller";
+import { fetchPreviewWallpapers } from "../controllers/wallpaper.controller";
 
 const storage = multer.memoryStorage(); // Store file in memory instead of disk
 const upload = multer({ storage });
 
 router.post('/saveWallpaper', upload.single('image'), saveWallpaper);
+router.post('/fetchPreviewWallpapers', fetchPreviewWallpapers);
 
 export default router;
