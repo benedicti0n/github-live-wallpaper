c7724ea8f562cca6577384aba2f51a3d5f6c713b
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-21 05:33:01 +0530

    feat: enhance WallpaperPreview and ConfirmDeleteModal with userId and platform support for deletion

--- frontend/src/components/Dashboard/AvailableWallpaper.tsx
+++ frontend/src/components/Dashboard/AvailableWallpaper.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import MiniButton from "../ui/MiniButton";
 import WallpaperPreview from "./WallpaperPreview";
 import { LucidePlus } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
 
 interface IPlatfromDetails {
     platform: string;
@@ -13,17 +14,20 @@ interface IPlatfromDetails {
 
 const AvailableWallpaper = (props: IPlatfromDetails) => {
     const navigate = useNavigate()
-    const platfrom = props.platform.toLowerCase()
+    const platform = props.platform.toLowerCase()
+
+    const { user } = useUser();
+    const userId = user?.id;
 
     return (
         <div className="h-full w-full rounded-2xl py-2 mt-2 flex items-center relative">
             {props.wallpapers && props.wallpapers
                 .filter(wallpaper => wallpaper.wallpaperId && wallpaper.link[0])
                 .map((wallpaper) => (
-                    <WallpaperPreview key={wallpaper.wallpaperId} imageUrl={wallpaper.link[0]} />
+                    <WallpaperPreview key={wallpaper.wallpaperId} wallpaperId={wallpaper.wallpaperId} imageUrl={wallpaper.link[0]} platformOf={platform} userId={userId} />
                 ))}
             <div className="h-full flex justify-center items-center">
-                <MiniButton variant="default" onClickFunction={() => navigate(`/${platfrom}/create`)}>
+                <MiniButton variant="default" onClickFunction={() => navigate(`/${platform}/create`)}>
                     <LucidePlus className="h-6 w-6" />
                 </MiniButton>
             </div>
--- frontend/src/components/Dashboard/WallpaperPreview.tsx
+++ frontend/src/components/Dashboard/WallpaperPreview.tsx
@@ -8,9 +8,15 @@ import ConfirmDeleteModal from "../ui/Modals/ConfirmDeleteModal"
 import { LucidePen, LucideTrash2 } from "lucide-react"
 
 const modalOpen = atom<boolean>(false)
-const wallpaperId = '1'
 
-const WallpaperPreview = ({ imageUrl }: { imageUrl: string }) => {
+interface IWallpaperPreview {
+    imageUrl: string;
+    wallpaperId: string;
+    platformOf: string;
+    userId: string | undefined;
+}
+
+const WallpaperPreview = (props: IWallpaperPreview) => {
     const navigate = useNavigate()
     const [isModalOpen, setIsModalOpen] = useAtom(modalOpen)
 
@@ -27,15 +33,15 @@ const WallpaperPreview = ({ imageUrl }: { imageUrl: string }) => {
 
     return (
         <div className="w-72 h-40 rounded-xl relative mr-6 border-2">
-            <img src={`${imageUrl}`} alt={`${imageUrl}`} className="bg-cover bg-center bg-no-repeat rounded-xl w-full h-full" onClick={previewWallpaper} />
+            <img src={`${props.imageUrl}`} alt={`${props.imageUrl}`} className="bg-cover bg-center bg-no-repeat rounded-xl w-full h-full" onClick={previewWallpaper} />
             <div className="absolute top-0 right-0 m-1 z-10">
-                <MiniButton variant="default" className="mx-1" onClickFunction={() => { navigate(`/edit/${wallpaperId}`) }}><LucidePen className="h-4 w-4" /></MiniButton>
+                <MiniButton variant="default" className="mx-1" onClickFunction={() => { navigate(`/edit/${props.wallpaperId}`) }}><LucidePen className="h-4 w-4" /></MiniButton>
                 <MiniButton variant="destructive" onClickFunction={openDeleteConfirmModal}><LucideTrash2 className="h-4 w-4" /></MiniButton>
             </div>
 
-            {isDeleteConfirmModalOpen && <ConfirmDeleteModal closeModal={() => setIsDeleteConfirmModal(false)} />}
+            {isDeleteConfirmModalOpen && <ConfirmDeleteModal closeModal={() => setIsDeleteConfirmModal(false)} wallpaperId={props.wallpaperId} platformOf={props.platformOf} userId={props.userId} />}
 
-            {isModalOpen && <PreviewModal imageUrl={imageUrl} closeModal={() => setIsModalOpen(false)} />}
+            {isModalOpen && <PreviewModal imageUrl={props.imageUrl} closeModal={() => setIsModalOpen(false)} />}
         </div>
     )
 }
