e163ea4b152fc1a8710fab21de19aa6aff609ddb
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-02-23 01:03:46 +0530

    feat: add daisyui for enhanced UI components and styling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,9 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
